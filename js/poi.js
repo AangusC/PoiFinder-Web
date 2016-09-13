a6a148222e765061c0bd13b42e2336d92af9115f
Laboratorio <Laboratorio>
2016-09-13 20:50:28 -0300

    cualquiercosa

--- js/poi.js
+++ js/poi.js
@@ -1,23 +1,31 @@
-function Poi(nombre,coordenada,horario,distanciaMinCercania){
+function Poi(nombre,coordenada,horario,horario2,distanciaMinCercania){
 	this.nombre= nombre;
 	this.coordenada=coordenada;
-	this.horario=horario;
+	this.horarioap=horario;
+	this.horariocie=horario2;
 	this.distanciaMinCercania=distanciaMinCercania;
+	self = this;
 
-
-}
-
-Poi.prototype= {
-estaCercaDe: function (point) {
-	var self = this;
-		return  self.coordenada.distance(point) <= self.distanciaMinCercania;
-	}
-
-//estaAbierto: function(nombre,dia,hora,minuto) {
-	//	(horarios.checkHora(hora, minuto));
-	//}
 };
-/*new Poi({nombre: "blah", coordenada: 2})
+
+
+
+Poi.prototype.estaCercaDe= function (point) {
+		return  self.coordenada.distance(point) <= self.distanciaMinCercania;
+	};
+
+Poi.prototype.estaAbierto= function(hora) {
+
+		return (hora >= self.horarioap)&&(hora<=self.horariocie);
+		//return	(self.checkHora(hora));
+	
+};
+
+/*function checkHora(hora){
+	var self = this;
+	return if(hora>=self.horarioap&&hora<=self.horariocie);
+}
+new Poi({nombre: "blah", coordenada: 2})
 
 function Poi(opts){
 	this.nombre= opts.nombre || "";
--- testJS/testPoi-test.js
+++ testJS/testPoi-test.js
@@ -6,24 +6,30 @@ describe("Test 1", function() {
 	var obelisco;
 	var poi;
 	var coordenadaBanco1;
+	var coordenadaBanco2;
 
 	beforeEach(function() {
 		bombonera = new Point(-34.63565, -58.36465);
 		comisaria24 = new Point(-34.6342281,-58.3603267);
 		obelisco = new Point(-34.603739,-58.38157);
-		poi = new Poi("Arnaldo",new Point(-34.546562, -58.556038),25,0.5);
+		poi = new Poi("Arnaldo",new Point(-34.546562, -58.556038),12,20,0.5);
 		coordenadaBanco1 = new Point(-34.548688, -98.552837);
+		coordenadaBanco2 = new Point(-34.548688, -58.552837);
 	});
 
 	
 
-	it("test 1", function() {
-		expect(poi.nombre).toEqual("Arnaldo");
+	it("test distancia verdadera", function() {
+		expect(poi.estaCercaDe(coordenadaBanco2)).toEqual(true);
 	});
-	it("test 2", function() {
+	it("test distancia falsa", function() {
 		console.log(poi.coordenada.distance(coordenadaBanco1));
 		console.log( poi.distanciaMinCercania);
 		expect(poi.estaCercaDe(coordenadaBanco1)).toEqual(false);
+
+	});
+	it("test estaAbierto", function() {
+		expect(poi.estaAbierto(13)).toEqual(true);
 
 	});
 
