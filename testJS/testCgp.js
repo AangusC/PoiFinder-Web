d4d0f2178423137a20c8350388caf3ab2c52390e
Laboratorio <Laboratorio>
2016-09-20 20:29:44 -0300

    CGP

--- testJS/testCgp.js
+++ testJS/testCgp.js
@@ -13,8 +13,20 @@ describe("Test Cgp", function() {
 		console.log(comuna.area)
 		expect(cgp.estaCercaDe(new Point(-34.513427, -58.479704))).toEqual(true);
 	});
+
 	it("fuera de la comuna", function() {		
-	
 		expect(cgp.estaCercaDe(new Point(-34.5427, -58.479704))).toEqual(false);
 	});
+
+
+	it("esta abierto", function() {	
+		var servicio1 = new Servicio({nombre:"Atencion al Vecino",horaApertura:12,horaCierre:20 });
+		var servicio2 = new Servicio({nombre:"Cuidado",horaApertura:14, horaCierre:21 });
+		var servicio3 = new Servicio({nombre:"Servicio de Rentas",horaApertura:08 , horaCierre:12 });
+		cgp.agregarServicio(servicio1);
+		cgp.agregarServicio(servicio2);
+		cgp.agregarServicio(servicio3);
+		expect(cgp.estaServicio("Cuidado")).toEqual(true);
+	});
+
 });
