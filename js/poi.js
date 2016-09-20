7247ea967aa569a5a10fe73844f9764df2f6f11e
Laboratorio <Laboratorio>
2016-09-20 18:57:40 -0300

    blee

--- js/poi.js
+++ js/poi.js
@@ -5,29 +5,38 @@ function Poi(myJson){
 	this.horariocie = myJson.horario2 || "";
 	this.distanciaMinCercania = myJson.distanciaMinCercania || 0.1;
 	this.direccion = myJson.direccion || "";
+	this.barrio = myJson.barrio ||"";				
+	this.numero = myJson.numero || 0;
+	this.diasAtencion = myJson.dias || ["lunes","martes","miercoles","jueves","viernes"];
+	this.palabrasClaves = myJson.Claves ||"";
+
 	self = this;
 
 	this.opiniones = [];
 
 	this.guardarOpinion = function(texto, user, puntaje) {
 		//opiniones.removeIf([o|o.user == user])
-		var op = new Opinion(texto, user, puntaje)
-		this.opiniones.push(op)
-	}
+		var op = new Opinion(texto, user, puntaje);
+		this.opiniones.push(op);
+	};
 
 	this.getPoints = function() {
 		var pun =0;
 		var len = this.opiniones.length;
 		if (len > 0 ) {
 			for (i = 0; i < len ; i++) {
-				pun = pun + this.opiniones[i].score
+				pun = pun + this.opiniones[i].score;
 			}
 			pun = pun / len;
 		}
         return pun;
-	}
+	};
+	this.getOpinion = function(user){
+		var tmp=this.opiniones.filter(op=> op.usuario.nombre == user.nombre);
+		return tmp[0];
+	};
 
-};
+}
 
 
 
