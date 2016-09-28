ecb37a17524f846f12bd1f11fcfc824ad6b6840a
DF4D133 <configurar@tecnosoft.com>
2016-09-28 15:32:42 -0300

    jorge: se sube angular en busquedaPoi y se agrega un binding para filtrar

--- js/app.js
+++ js/app.js
@@ -22,10 +22,31 @@ poiApp.controller('loginCtrl', function() {
          }catch (exception) {
             loginForm.$invalid = true;
             this.errorMessage = exception;
-        }
-         
-        
-    };
+        }             
+    };       
+});
+
+poiApp.controller('busquedaCtrl', function () {
+    var self=this;
+    self.textoBusqueda = '';  
+    self.filtroActual = '';
+
+    self.listaPios = [
+        {
+            "nombre" : "Galicia",
+            "direccion" : "Puerredon",
+            "cercania" : "No",
+            "favorito" : "☻"
+        },
+        { "nombre" : "CGP 1", "direccion" : "Lavalleja 582", "cercania" : "No", "favorito" : "º_º"},
+        { "nombre" : "Santander", "direccion" : "Cochabamba 2219", "cercania" : "No", "favorito" : "º_º"},
+        { "nombre" : "Linea 86", "direccion" : "Calle falsa 123", "cercania" : "Si", "favorito" : "º_º"}
+    ];
+
     
-   
+
+    self.buscar = function() {
+        self.filtroActual = self.textoBusqueda
+    };
+
 });
