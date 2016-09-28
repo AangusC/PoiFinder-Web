88c29673170772c424064233d2c75773706996a5
DF4D133 <configurar@tecnosoft.com>
2016-09-28 12:04:42 -0300

    jorge:subo el packaje.json y pongo angular en login.html pero falta la logica del controler en app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+'use strict';
+var poiApp = angular.module('poiApp', []);
+
+poiApp.controller('loginCtrl', function() {
+    var self=this;
+    self.user="";
+    self.pass="";
+    
+    self.validarNombre= function(){
+        console.log("test");
+    }
+    
+   
+});
