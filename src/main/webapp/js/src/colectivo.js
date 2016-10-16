f4f8c6a9faaa65106ef2e7db4e0c5258b91b4db5
Aangusc <carozzi.gustavo@gmail.com>
2016-10-15 21:36:39 -0300

    colectivo arreglado

--- src/main/webapp/js/src/colectivo.js
+++ src/main/webapp/js/src/colectivo.js
@@ -2,11 +2,11 @@
 var Colectivo = Poi.extend(function (op) {
 	this.nombre= op.nombre  || "";
 	var paradaJson=[]
-	for(var i = 0, len = op.parada; i < len; i++){
-		var temp =op.parada[i];
+	for(var i = 0; i < op.paradas.length; i++){
+		var temp =op.paradas[i];
 		var tempX = temp.x;
 		var tempY = temp.y;
-		this.paradaJson.push(new Point(tempX,tempY));
+		paradaJson.push(new Point(tempX,tempY));
 	}
 	this.paradas= paradaJson;
 	console.log(this.paradas)
@@ -17,12 +17,15 @@ var Colectivo = Poi.extend(function (op) {
 		return _.some(self.paradas, function(parada){parada.distance(point) <= 0.1});
 	},
 	distancia: function(point){
-		var tempa=this.paradas[0].distance(point);
+		/*var tempa=self.paradas[0].distance(point);
 		 for(var i=1;i<this.paradas.size;i++){
 		 	if(this.paradas[i].distance(point)< tempa){
 		 		tempa=this.paradas[i].distance(point);
 		 	}
 		 }
-		return tempa;
+		return tempa;*/
+		var temp= []
+		_.each(self.paradas,function(parada){temp.push(parada.distance(point))})
+		return parseFloat(Math.round(_.min(temp) * 100) / 100).toFixed(2)
 	}
   });
